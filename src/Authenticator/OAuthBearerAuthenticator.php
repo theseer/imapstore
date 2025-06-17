5a355532a1a0a5f3937f26b9a75d2a2fcb5aac3b
Arne Blankerts <Arne@Blankerts.de>
2025-06-17 22:56:03 +0200

    Add OAuthBearerAuthenticator

---
+++ src/Authenticator/OAuthBearerAuthenticator.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+namespace theseer\imapstore;
+
+use function base64_encode;
+use function sprintf;
+use function str_contains;
+
+final readonly class OAuthBearerAuthenticator implements Authenticator {
+
+    public function __construct(
+        private string $username,
+        private string $accessToken
+    ) {
+    }
+
+    public function authenticate(Connection $connection): void {
+        $xoauth = base64_encode(
+            sprintf("user=%s\x01auth=Bearer %s\x01\x01",
+                $this->username,
+                $this->accessToken
+            )
+        );
+
+        $response = $connection->writeCommand(
+            sprintf('AUTHENTICATE XOAUTH2 %s', $xoauth)
+        );
+        if (!str_contains($response, "OK")) {
+            throw new AuthenticatorException(
+                "Authentication failed: $response"
+            );
+        }
+    }
+}
